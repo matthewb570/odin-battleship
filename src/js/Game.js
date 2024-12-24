fe2b64975c63dec1c561a8c63721ef3281cb40fe
Matthew Burgess <boog397@gmail.com>
2024-12-23 22:46:17 -0600

    Add Game class with temporary setup logic

---
+++ src/js/Game.js
@@ -0,0 +1,18 @@
+import Player from "./Player";
+
+export default class Game {
+    
+    player1;
+    player2;
+
+    constructor() {
+        this.player1 = new Player(true);
+        this.player2 = new Player(false);
+
+        this.player1.gameBoard.placeShip([0, 0], 5, "vertical");
+        this.player1.gameBoard.placeShip([5, 5], 3, "horizontal");
+
+        this.player2.gameBoard.placeShip([0, 0], 5, "horizontal");
+        this.player2.gameBoard.placeShip([5, 5], 3, "vertical");
+    }
+}
