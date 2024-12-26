992d7140ac82f09c3dcc7e23830bbf1dce4b2a1e
Matthew Burgess <boog397@gmail.com>
2024-12-26 16:52:11 -0600

    Add logic to disable game boards based on the current turn

--- src/js/GameBoardDisplay.js
+++ src/js/GameBoardDisplay.js
@@ -1,21 +1,26 @@
 export default class GameBoardDisplay {
 
-    static draw(gameBoard, container, reDrawFunction) {
+    static draw(gameBoard, container, reDrawFunction, isDisabled) {
         let divGameBoard = document.createElement("div");
         divGameBoard.classList.add("game-board");
-        divGameBoard.appendChild(this.#createTileList(gameBoard, reDrawFunction));
+        if (isDisabled) {
+            divGameBoard.classList.add("disabled");
+        }
+        divGameBoard.appendChild(this.#createTileList(gameBoard, reDrawFunction, isDisabled));
 
         container.appendChild(divGameBoard);
     }
 
-    static #createTileList(gameBoard, reDrawFunction) {
+    static #createTileList(gameBoard, reDrawFunction, isDisabled) {
         let divTileList = document.createElement("div");
         divTileList.classList.add("tile-list");
         for (let j = 0; j < gameBoard.getGameBoardWidth(); j++) {
             for (let i = 0; i < gameBoard.getGameBoardHeight(); i++) {
                 let clickHandler = () => {
-                    gameBoard.receiveAttack([i, j]);
-                    reDrawFunction();
+                    if (!isDisabled) {
+                        gameBoard.receiveAttack([i, j]);
+                        reDrawFunction();
+                    }
                 };
                 divTileList.appendChild(this.#createTile(gameBoard.areCoordinatesAttacked([i, j]), gameBoard.doesShipExist([i, j]), clickHandler));
             }
--- src/js/GameDisplay.js
+++ src/js/GameDisplay.js
@@ -22,12 +22,12 @@ export default class GameDisplay {
     static #createLabeledGameBoardList(game, reDrawFunction) {
         let divLabeledGameBoardList = document.createElement("div");
         divLabeledGameBoardList.classList.add("labeled-game-board-list");
-        divLabeledGameBoardList.appendChild(this.#createLabeledGameBoard(game.player1.name, game.player1.gameBoard, reDrawFunction));
-        divLabeledGameBoardList.appendChild(this.#createLabeledGameBoard(game.player2.name, game.player2.gameBoard, reDrawFunction));
+        divLabeledGameBoardList.appendChild(this.#createLabeledGameBoard(game.player1.name, game.player1.gameBoard, reDrawFunction, game.currentTurn !== 0));
+        divLabeledGameBoardList.appendChild(this.#createLabeledGameBoard(game.player2.name, game.player2.gameBoard, reDrawFunction, game.currentTurn !== 1));
         return divLabeledGameBoardList;
     }
 
-    static #createLabeledGameBoard(playerName, gameBoard, reDrawFunction) {
+    static #createLabeledGameBoard(playerName, gameBoard, reDrawFunction, isDisabled) {
         let divPlayerName = document.createElement("div");
         divPlayerName.classList.add("player-name");
         divPlayerName.textContent = playerName;
@@ -35,7 +35,7 @@ export default class GameDisplay {
         let divLabeledGameBoard = document.createElement("div");
         divLabeledGameBoard.classList.add("labeled-game-board");
         divLabeledGameBoard.appendChild(divPlayerName);
-        GameBoardDisplay.draw(gameBoard, divLabeledGameBoard, reDrawFunction);
+        GameBoardDisplay.draw(gameBoard, divLabeledGameBoard, reDrawFunction, isDisabled);
 
         return divLabeledGameBoard;
     }
