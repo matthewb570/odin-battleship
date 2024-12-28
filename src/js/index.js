a0f27ac766fcfe46e742e6a8a9a6b4bebb8a6325
Matthew Burgess <boog397@gmail.com>
2024-12-27 19:38:00 -0600

    Rename class for main div in index.html

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import "../css/styles.css";
 import Game from "./Game.js";
 import GameDisplay from "./GameDisplay.js";
 
-const divGame = document.querySelector(".game");
+const divGame = document.querySelector(".content");
 
 let game = new Game();
 GameDisplay.draw(game, divGame);
